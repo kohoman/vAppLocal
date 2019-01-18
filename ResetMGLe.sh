60088ab7126d5f597f0adc1ac7faca6d8d9be8ce
K. O. Homan <khoman@mst.edu>
2019-01-18 08:36:08 -0600

    Added scripts to reset the vApp to the state preceding execution of the SetupM??e.sh script.  Primarily a debugging aid.

---
+++ ResetMGLe.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+--- Script to reset setup steps for MGLe ---
+
+# ... symbolic links ...
+
+cd $HOME
+rm -f .bashrc
+rm -f .bash_profile
+rm -f .emacs
+
+# ... directories ...
+
+rm -rf bin
+rm -rf texmf
+rm -rf Desktop/References
+rm -rf Documents/mweLaTeX
+rm -rf Documents/mstTheses
+rm -rf .unison
---
+++ ResetMPCe.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+--- Script to reset setup steps for MGLe ---
+
+# ... symbolic links ...
+
+cd $HOME
+rm -f .bashrc
+rm -f .bash_profile
+rm -f .emacs
+
+# ... directories ...
+
+rm -rf bin
+rm -rf texmf
+rm -rf python
+rm -rf Desktop/References
+rm -rf Documents/mweLaTeX
+rm -rf Documents/mstTheses
+rm -rf .unison
